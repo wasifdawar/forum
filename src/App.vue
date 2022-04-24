58b236af43de3c52e8358dd03635ab9bbb941875
wasif dawar <m.wasifdawar@gmail.com>
2022-04-23 23:04:12 -0400

    Feat: Display list of elements

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <page-home />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import PageHome from "@/components/PageHome";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    PageHome,
+  },
+};
 </script>
 
 <style>
